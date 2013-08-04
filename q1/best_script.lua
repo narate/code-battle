1600cdb1823c8e9d0f95ae972d4bdfd26d01f529
Narate Ketram <koonnarate@gmail.com>
2013-08-04 13:54:51 +0700

    First commit

---
+++ q1/best_script.lua
@@ -0,0 +1,26 @@
+local fin = io.open('numbers.txt', 'r')
+local fout = io.open('run_result.txt', 'w')
+local din = {}
+
+if fin then
+    for line in fin:lines() do
+        if #line ~= 0 then
+            table.insert(din,line)
+        end
+    end
+    table.sort(din)
+    local count, before, current, diff = 0, -1, 0, 0
+    for i,current in ipairs(din) do
+        diff = current - before
+        while diff > 1 do
+            fout:write(string.format("%04d\n", count));
+            diff = diff - 1
+            count = count + 1
+        end
+        before = current
+        count = count + 1
+    end
+    fin:close()
+    fout:close()
+end
+print('done :)')
---
+++ q2/best_script.py
@@ -0,0 +1,18 @@
+fin = open('numbers.txt', 'r')
+fout = open('run_result.txt', 'w')
+din = []
+for line in fin.readlines():
+    din.append(int(line.strip()))
+
+dout = {}
+for i in range(0,int(max(din)) + 1):
+    dout[i] = 0
+
+for i in din:
+    dout[i] = dout[i] + 1
+        
+for k, v in dout.items():
+    if v > 1:
+        fout.write("\"" + str(k) + "\", " + str(v) + "\n")
+fin.close()
+fout.close()
