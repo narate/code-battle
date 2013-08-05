0f47aab4892e8630797eb8688f2ba56b7e33027e
Narate Ketram <koonnarate@gmail.com>
2013-08-05 10:16:46 +0700

    Fixed q2/best_script.py

--- q2/best_script.py
+++ q2/best_script.py
@@ -9,15 +9,15 @@ fin = open('numbers.txt', 'r')
 fout = open('run_result.txt', 'w')
 din = []
 for line in fin.readlines():
-    din.append(int(line.strip()))
-
+    din.append(line.strip())
+    
 dout = {}
-for i in range(0,int(max(din)) + 1):
+for i in din:
     dout[i] = 0
 
-for i in din:
-    dout[i] = dout[i] + 1
-        
+for k in din:
+    dout[k] = dout[k] + 1
+    
 for k, v in dout.items():
     if v > 1:
         fout.write("\"" + str(k) + "\", " + str(v) + "\n")
